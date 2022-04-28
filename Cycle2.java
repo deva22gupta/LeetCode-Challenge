8fb93f3fe9ff90cf31f63b6e65ddf7a233f3b06b
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-04-29 01:49:40 +0530

    Its also returning the intersected node
    also checking whether it has loop or not

--- Cycle2.java
+++ Cycle2.java
@@ -53,48 +53,7 @@ public static Node detectCycle(Node head) {
     return null;
 }
 
-// Function to detect and remove loop
-// in a linked list that may contain loop
-// static Node detectAndRemoveLoop(Node head)
-// {
-// // If list is empty or has
-// // only one node without loop
-// if (head == null || head.next == null)
-// 	return null;
 
-// Node slow = head, fast = head;
-
-// // Move slow and fast 1
-// // and 2 steps ahead
-// // respectively.
-// slow = slow.next;
-// fast = fast.next.next;
-
-// // Search for loop using
-// // slow and fast pointers
-// while (fast != null && fast.next != null)
-// {
-// 	if (slow == fast)
-// 	break;
-// 	slow = slow.next;
-// 	fast = fast.next.next;
-// }
-
-// // If loop does not exist
-// if (slow != fast)
-// 	return null;
-
-// // If loop exists. Start slow from
-// // head and fast from meeting point.
-// slow = head;
-// while (slow != fast)
-// {
-// 	slow = slow.next;
-// 	fast = fast.next;
-// }
-
-// return slow;
-// }
 
 // Driver code
 
