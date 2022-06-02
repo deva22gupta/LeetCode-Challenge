f2235f64b7353c56ca4eeba46325a2896f44c9f2
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-03 01:04:14 +0530

    Time: 52 ms (56.53%), Space: 106 MB (46.55%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -10,12 +10,17 @@ class Solution {
         // if we use HashMap
         
             int eligible= A.length/2;
-        HashMap<Integer,Integer> hs = new HashMap<>();
+       // HashMap<Integer,Integer> hs = new HashMap<>();
         
         
         
+        // for(int i=0;i<A.length;i++){
+        //     hs.put(A[i],hs.getOrDefault(A[i],0)+1);
+        // }
+        
+        HashSet<Integer> hs = new HashSet<>();
         for(int i=0;i<A.length;i++){
-            hs.put(A[i],hs.getOrDefault(A[i],0)+1);
+            hs.add(A[i]);
         }
         return Math.min(hs.size(),eligible);
         // no of different types of candies
