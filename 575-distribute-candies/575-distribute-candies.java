8bd9ae0c7f45dc1e0c08cc0418365864e2848476
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-03 01:01:46 +0530

    Time: 89 ms (7.27%), Space: 116.8 MB (6.37%) - LeetHub

---
+++ 575-distribute-candies/575-distribute-candies.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int distributeCandies(int[] A) {
+     
+        // you are given different types of candies (the candies count is equal to the length // of the array)
+        
+        // return the maximum number of different types of candies she can eat if she only 
+        // eats n / 2 of them
+    
+        // count the no of types of candies
+        // if we use HashMap
+        
+            int eligible= A.length/2;
+        HashMap<Integer,Integer> hs = new HashMap<>();
+        
+        
+        
+        for(int i=0;i<A.length;i++){
+            hs.put(A[i],hs.getOrDefault(A[i],0)+1);
+        }
+        return Math.min(hs.size(),eligible);
+        // no of different types of candies
+        
+        
+    }
+}
