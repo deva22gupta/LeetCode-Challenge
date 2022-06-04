bca70f467d23d1c72fcf446a5e1e9fea7fe59824
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-04 23:48:31 +0530

    Time: 188 ms (42.36%), Space: 127.9 MB (50.36%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,33 +1,25 @@
 class NumMatrix {
 
-    int [][]sum;
+    int[][] matrix;
     public NumMatrix(int[][] matrix) {
-        int row=matrix.length;
-        int col=matrix[0].length;
-        sum = new int[row+1][col+1];
-        for(int i=1;i<=row;i++){
-            for(int j=1;j<=col;j++){
-                
-                // summing the matrix and writing it at the lower right bottom
-                sum[i][j]=sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i-1][j-1];
-                
-                
+        for(int i=0;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                matrix[i][j] += matrix[i][j-1];
             }
         }
         
+        for(int i=1;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j] += matrix[i-1][j];
+            }
+        }
         
+        this.matrix = matrix;
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        row1++;col1++;row2++;col2++;
-        return sum[row2][col2]-sum[row1-1][col2]-sum[row2][col1-1]+sum[row1-1][col1-1];
-        
-      //  return result;
+        int total = matrix[row2][col2];
+        int extra = (col1!=0?matrix[row2][col1-1]:0) + (row1!=0?matrix[row1-1][col2]:0) - ((row1!=0 && col1!=0)?matrix[row1-1][col1-1]:0);
+        return total - extra;
     }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
