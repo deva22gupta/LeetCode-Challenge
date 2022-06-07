a49aabe2fb7cb525f91da2e41d05811f2e1db297
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-08 01:31:06 +0530

    Time: 2 ms (99.53%), Space: 42.9 MB (89.59%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -4,12 +4,16 @@ class Solution {
         // go from left
         /// go from right
         // take the max no ofit
-         int n =A.length;
+       // given are the ratings of the candidates in the input array
+      // forming left and right array and taking the max out of it 
+
+      int n =A.length;
       int left[]= new int [n];
-      int right[]= new int [n];
-      int result[]= new int[n];
+     // int right[]= new int [n];
+     int right=1;
+     // int result[]= new int[n];
       Arrays.fill(left,1);
-      Arrays.fill(right,1);
+    //  Arrays.fill(right,1);
       for(int i=1;i<n;i++){
           if(A[i-1]<A[i]){
               left[i]=left[i-1]+1;
@@ -17,19 +21,22 @@ class Solution {
       }
       for(int i=n-1;i>0;i--){
           if(A[i]<A[i-1]){
-              right[i-1]=right[i]+1;
+              right++;
+              left[i-1]=Math.max(left[i-1],right);
           }
+          else right=1;
       }
 
       // take the max
-      for(int i=0;i<n;i++){
-          result[i] = Math.max(left[i],right[i]);
-      }
+    //   for(int i=0;i<n;i++){
+    //       result[i] = Math.max(left[i],right[i]);
+    //   }
       int sum=0;
       for(int i=0;i<n;i++){
-          sum+=result[i];
+          sum+=left[i];
       }
       return sum;
+
 
 
     }
