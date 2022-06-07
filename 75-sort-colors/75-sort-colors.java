935fb382bf2c831b78e93a5f2623b427e23fa577
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-07 23:28:18 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (5.30%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,43 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+    public void sortColors(int[] arr) {
+       // Arrays.sort(arr);
+        // two pointer's approach 
+        int l=0,h=arr.length-1,mid=0,temp=0;
+        for(int i=0;i<arr.length;i++){
+            switch(arr[mid]){
+                case 0:{
+                    // swap with the first
+                    temp=arr[l];
+                    arr[l]=arr[mid];
+                    arr[mid]=temp;
+                    l++;
+                    mid++;
+                    break;
+                }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    
+                    temp=arr[h];
+                    arr[h]=arr[mid];
+                    arr[mid]=temp;
+                    h--;
+                  //  mid++;
+                    break;
+                }
+                        
+                        
+                        
+                        
+                        
+                    
+            }
+            
+        }
+        
+        
+        
     }
 }
