b30b219951ff9a0fcf29f8210e4a544f45bb00b2
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-08 17:13:11 +0530

    Time: 15 ms (63.24%), Space: 45.3 MB (72.29%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,39 @@
+class Solution {
+   
+        // based onfloyd warshall
+        // can try it using dikstra  as negative weight is not given so try with that
+        
+        //    Time O(N^3)
+  //  Space O(N^2)
+
+
+  // its using floyd warshall algo or try it by using dijkstra
+
+
+
+    public static int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] dis = new int[n][n];
+        int res = 0, smallest = n;
+        for (int[] row : dis)
+            Arrays.fill(row, 10001);    // as distance is upto 10^4
+        for (int[] e : edges)
+            dis[e[0]][e[1]] = dis[e[1]][e[0]] = e[2];             // filling out  the distance for non directional graph
+        for (int i = 0; i < n; ++i)
+            dis[i][i] = 0;                       //  distance from itself is zero
+        for (int k = 0; k < n; ++k)
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                    dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; ++j){
+                if (dis[i][j] <= distanceThreshold)
+                    ++count; }
+            if (count <= smallest) {
+                res = i;
+                smallest = count;
+            }
+        }
+        return res;
+    }
+}
