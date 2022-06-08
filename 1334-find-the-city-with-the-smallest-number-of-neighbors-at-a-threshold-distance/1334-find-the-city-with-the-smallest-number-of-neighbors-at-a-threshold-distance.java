50a22882c88e198a0d53d9c9982188d5ff66afcc
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-08 17:40:17 +0530

    Time: 11 ms (78.19%), Space: 45.5 MB (70.64%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -15,7 +15,7 @@ class Solution {
         int[][] dis = new int[n][n];
         int res = 0, smallest = n;
         for (int[] row : dis)
-            Arrays.fill(row, 10001);    // as distance is upto 10^4
+            Arrays.fill(row, 10001);    // as distance is upto 10^4 (here see the dstance array)   n is upto 10^2
         for (int[] e : edges)
             dis[e[0]][e[1]] = dis[e[1]][e[0]] = e[2];             // filling out  the distance for non directional graph
         for (int i = 0; i < n; ++i)
