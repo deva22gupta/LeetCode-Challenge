1b5a206fc346cf0230c312891685875f1cbbe48d
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-11 03:37:55 +0530

    Time: 4 ms (90.61%), Space: 42.4 MB (92.85%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,32 +1,23 @@
 class Solution {
    
-        public boolean canFinish(int numCourses, int[][] prerequisites) {
-    int[][] matrix = new int[numCourses][numCourses]; // i -> j
-    int[] indegree = new int[numCourses];
-    
-    for (int i=0; i<prerequisites.length; i++) {
-        int ready = prerequisites[i][0];
-        int pre = prerequisites[i][1];
-        if (matrix[pre][ready] == 0)
-            indegree[ready]++; //duplicate case
-        matrix[pre][ready] = 1;
-    }
-    
-    int count = 0;
-    Queue<Integer> queue = new LinkedList();
-    for (int i=0; i<indegree.length; i++) {
-        if (indegree[i] == 0) queue.offer(i);
-    }
-    while (!queue.isEmpty()) {
-        int course = queue.poll();
-        count++;
-        for (int i=0; i<numCourses; i++) {
-            if (matrix[course][i] != 0) {
-                if (--indegree[i] == 0)
-                    queue.offer(i);
-            }
+      public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] neigh = new LinkedList[numCourses];
+        Queue<Integer> queue = new LinkedList<>();
+        int[] indegree = new int[numCourses];
+        int count = 0;
+        for (int i = 0; i < numCourses; i++) neigh[i] = new LinkedList<>();
+        for (int[] pair : prerequisites) {
+            neigh[pair[1]].add(pair[0]);
+            indegree[pair[0]]++;
         }
+        for (int i = 0; i < indegree.length; i++)
+            if (indegree[i] == 0) queue.offer(i);
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            for (int adj : neigh[course])
+                if (--indegree[adj] == 0) queue.offer(adj);
+        }
+        return count == numCourses;
     }
-    return count == numCourses;
-}
     }
