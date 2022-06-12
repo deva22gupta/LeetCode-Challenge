e297e42d450fc45e4a216df7cb34dc6c1458ae88
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-13 01:23:56 +0530

    Time: 6 ms (23.74%), Space: 104.1 MB (10.60%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double averageWaitingTime(int[][] A) {
+     
+        // first come first serve nonpremptive
+        
+//         for(int i=0;i<customers.length;i++){
+//           //  for(int j=0;j<customers.length[0];j++){
+//                 int ar=customers[i][0];
+//                 tot=ar+customers[i][1];
+//                 if(tot<nextarr){
+//                     chef can start
+//                 }
+                
+//             }
+        
+          double wait = 0, cur = 0;
+        for (int[] a: A) {
+            cur = Math.max(cur,  a[0]) + a[1];
+            wait += cur - a[0];
+        }
+        return 1.0 * wait / A.length;
+        
+        
+        }
+        
+    }
