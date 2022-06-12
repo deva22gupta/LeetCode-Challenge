912e24b30536be1f326171c9b8e47edf065ecf93
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-12 13:43:34 +0530

    Time: 6 ms (98.90%), Space: 88.4 MB (71.49%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,27 +1,36 @@
 class Solution {
-    public int maximumUniqueSubarray(int[] arr) {
-        int n = arr.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int j = 0;
-        int sum = 0;
-        int max = 0;
+     public int maximumUniqueSubarray(int[] nums) {
         
-        for(int i=0; i<n; i++){
-            sum += arr[i];
-            if(map.containsKey(arr[i])==false){
-                map.put(arr[i],1);
-            }
-            else{
-                while(arr[j]!=arr[i]){
-                    map.remove(arr[j]);
-                    sum -=arr[j];
-                    j++;
+        final int n = nums.length;
+        
+        // stores if num is visited
+        final boolean[] visited = new boolean[(int) (1e4 + 1)];
+
+        int currSum = 0, maxSum = 0;
+        
+        for (int i=0, j=0; j<n; j++) {
+            
+            // checks if current element is unvisited
+            if (!visited[nums[j]]) {
+                currSum += nums[j]; // add to current sum
+                visited[nums[j]] = true; // mark as visited
+            } else {
+                
+                // if visited ele found then compare with maxSum so far
+                if (maxSum < currSum) maxSum = currSum;
+                
+                // remove all elements (sum) before current duplicate element 
+                while (nums[i] != nums[j]) {
+                    visited[nums[i]] = false; // It is guaranteed that there are no duplicate elements present before current ele, so remove all elements and mark visited as false
+                    currSum -= nums[i++];
                 }
-                sum -= arr[j];
-                j++;
+                
+                // shift one index as i is currently in duplicate element
+                i++;   
             }
-            max = Math.max(sum,max);
-        }
-        return max;
+        }        
+        
+        // handle last currSum since we only update maxSum on duplicate
+        return maxSum < currSum ? currSum : maxSum;
     }
 }
