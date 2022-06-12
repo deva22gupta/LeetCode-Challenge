d02395c6f50bf37f6a65b2c2b8eb722c4caf6c16
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-12 17:16:03 +0530

    Time: 64 ms (37.66%), Space: 84.4 MB (64.01%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,20 +1,28 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-    
+        
         int n = nums.length;
-        Deque<Integer> dq = new ArrayDeque<>(); // store index of `nums` elements, elements is in decreasing order, the front is the maximum element.
-        dq.offer(0);
+        Deque<Integer> deque = new ArrayDeque<>(); 
+        deque.offer(0);
         for (int i = 1; i < n; ++i) {
-            // nums[i] = max(nums[i-k], nums[i-k+1], .., nums[i-1]) + nums[i] = nums[dq.front()] + nums[i]
-            nums[i] = nums[dq.peekFirst()] + nums[i];
+            int firstIndex = deque.peekFirst();
+            int firstValue = nums[firstIndex];
+            nums[i] = firstValue + nums[i];
 
-            // Add nums[i] to our deque
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) dq.pollLast(); // Eliminate elements less or equal to nums[i], which will never be chosen in the future
-            dq.offerLast(i);
+            while (!deque.isEmpty()) {
+                int lastIndex = deque.peekLast();
+                int lastValue = nums[lastIndex];
+                int currentValue = nums[i];
+                if ((lastValue > currentValue))
+                    break;
+                deque.pollLast();
+            }
+            deque.offerLast(i);
 
-            // Remove if the last element is out of window size k
-            if (i - dq.peekFirst() >= k) dq.pollFirst();
+            int differenceFromFirstIndex = i - deque.peekFirst();
+            if (differenceFromFirstIndex >= k)
+                deque.pollFirst();
         }
         return nums[n - 1];
+    }   
     }
-}
