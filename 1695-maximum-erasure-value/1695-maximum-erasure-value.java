4ab91d573b0400d7eeb4990e0ad8f2b5ad36059b
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-12 13:44:43 +0530

    Time: 7 ms (97.37%), Space: 51.3 MB (97.37%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -1,36 +1,21 @@
 class Solution {
-     public int maximumUniqueSubarray(int[] nums) {
-        
-        final int n = nums.length;
-        
-        // stores if num is visited
-        final boolean[] visited = new boolean[(int) (1e4 + 1)];
+   public int maximumUniqueSubarray(int[] nums) {
+    boolean[] vis = new boolean[10001];
+    int i = 0, j = 0, sum = 0, res = 0;
 
-        int currSum = 0, maxSum = 0;
-        
-        for (int i=0, j=0; j<n; j++) {
-            
-            // checks if current element is unvisited
-            if (!visited[nums[j]]) {
-                currSum += nums[j]; // add to current sum
-                visited[nums[j]] = true; // mark as visited
-            } else {
-                
-                // if visited ele found then compare with maxSum so far
-                if (maxSum < currSum) maxSum = currSum;
-                
-                // remove all elements (sum) before current duplicate element 
-                while (nums[i] != nums[j]) {
-                    visited[nums[i]] = false; // It is guaranteed that there are no duplicate elements present before current ele, so remove all elements and mark visited as false
-                    currSum -= nums[i++];
-                }
-                
-                // shift one index as i is currently in duplicate element
-                i++;   
-            }
-        }        
-        
-        // handle last currSum since we only update maxSum on duplicate
-        return maxSum < currSum ? currSum : maxSum;
+    while(j < nums.length){
+        while(j < nums.length && !vis[nums[j]]){//expand the window
+            vis[nums[j]] = true;
+            sum += nums[j++];
+        }
+
+        res = Math.max(res, sum);
+
+        while(j < nums.length && vis[nums[j]]){//shrink the window
+            vis[nums[i]] = false;
+            sum -= nums[i++];
+        }
     }
+    return res;
+}
 }
