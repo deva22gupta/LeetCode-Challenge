13c55ab75d8eb54aeaa432b4083141d2e0a467b7
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-13 01:24:36 +0530

    Time: 6 ms (23.74%), Space: 89.9 MB (75.76%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -16,7 +16,7 @@ class Solution {
           double wait = 0, cur = 0;
         for (int[] a: A) {
             cur = Math.max(cur,  a[0]) + a[1];
-            wait += cur - a[0];
+            wait += cur - 1.0 * a[0];
         }
         return  wait / A.length;
         
