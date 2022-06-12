c5eeebd11166c6119bb6b2346587f66e9f56e29c
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-12 17:22:06 +0530

    Time: 112 ms (73.58%), Space: 76.1 MB (84.91%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
@@ -1,34 +1,55 @@
+class UnionFind {
+    int[] parents;
+    public UnionFind(int size) {
+        parents = new int[size];
+        for (int i = 0; i < size; i++) {
+            parents[i] = i;
+        }
+    }
+    
+    public int find(int node) {
+        List<Integer> path = new ArrayList<>();
+        while (node != parents[node]) {
+            path.add(node);
+            node = parents[node];
+        }
+        for (int p : path) {
+            parents[p] = node;
+        }
+        return node;
+    }
+    
+    public void union(int node1, int node2) {
+        int root1 = find(node1);
+        int root2 = find(node2);
+        if (root1 != root2) {
+            parents[root1] = root2;
+        }
+    }
+}
 class Solution {
     public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
-      
-        int M = edgeList.length, N = queries.length;
-        DSU dsu = new DSU(n);
-        for (int i = 0; i < queries.length; i++) 
-            queries[i] = new int[]{queries[i][0], queries[i][1], queries[i][2], i}; 
-        Arrays.sort(queries, (a, b) -> a[2] - b[2]);
-        Arrays.sort(edgeList, (a, b) -> a[2] - b[2]);
-        boolean[] res = new boolean[N];
-        for (int i = 0, j = 0; i < N; i++) {
-            int[] query = queries[i];
-            while (j < M && edgeList[j][2] < queries[i][2])
-                dsu.union(edgeList[j][0], edgeList[j++][1]);
-            res[queries[i][3]] = dsu.find(queries[i][0]) == dsu.find(queries[i][1]);
+        List<int[]> reorderedQueries = new ArrayList<>();
+        for (int i = 0; i < queries.length; i++) {
+            reorderedQueries.add(new int[]{queries[i][0], 
+                                           queries[i][1], 
+                                           queries[i][2],
+                                           i});
+        }
+        Collections.sort(reorderedQueries, (a, b) -> Integer.compare(a[2], b[2]));        
+        Arrays.sort(edgeList, (a, b) -> Integer.compare(a[2], b[2]));
+        
+        UnionFind uf = new UnionFind(n);
+        boolean[] res = new boolean[queries.length];
+        for (int i = 0, j = 0; j < reorderedQueries.size(); j++) {
+            while (i < edgeList.length && edgeList[i][2] < reorderedQueries.get(j)[2]) {                
+                uf.union(edgeList[i][0], edgeList[i][1]);
+                i++;
+            }
+            if (uf.find(reorderedQueries.get(j)[0]) == uf.find(reorderedQueries.get(j)[1])) {
+                res[reorderedQueries.get(j)[3]] = true;
+            }
         }
         return res;
     }
 }
-
-class DSU {
-    int[] parent;
-    public DSU(int N) {
-        this.parent = new int[N];
-        for (int i = 0; i < N; i++) parent[i] = i;
-    }
-    public int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    public void union(int x, int y) {
-        parent[find(x)] = parent[find(y)];
-    }
-    }
