ffed608ec4d7d0ca46b8d6f3d76d29c82b277c4b
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-13 01:44:10 +0530

    Time: 53 ms (64.71%), Space: 80.4 MB (50.00%) - LeetHub

--- 1702-maximum-binary-string-after-change/1702-maximum-binary-string-after-change.java
+++ 1702-maximum-binary-string-after-change/1702-maximum-binary-string-after-change.java
@@ -1,19 +1,38 @@
 class Solution {
-    public String maximumBinaryString(String binary) {
+    public String maximumBinaryString(String s) {
         
-        int cnt0=0,leading1=0;
-        for(char ch:binary.toCharArray()) {
-            if(ch=='0') {
-                cnt0++;
-            } else if(cnt0==0) {
-                leading1++;   
-            }
+        // int cnt0=0,leading1=0;
+        // for(char ch:binary.toCharArray()) {
+        //     if(ch=='0') {
+        //         cnt0++;
+        //     } else if(cnt0==0) {
+        //         leading1++;   
+        //     }
+        // }
+        // if(cnt0<2) return binary;
+        // var chs=new char[binary.length()];
+        // Arrays.fill(chs,'1');
+        // chs[cnt0+leading1-1]='0';
+        // return new String(chs);
+        
+        
+        
+        
+        
+        
+            //public String maximumBinaryString(String s) {
+        int ones = 0, zeros = 0, n = s.length();
+        StringBuilder res = new StringBuilder("1".repeat(n));
+        for (int i = 0; i < n; ++i) {
+            if (s.charAt(i) == '0')
+                zeros++;
+            else if (zeros == 0)
+                ones++;
         }
-        if(cnt0<2) return binary;
-        var chs=new char[binary.length()];
-        Arrays.fill(chs,'1');
-        chs[cnt0+leading1-1]='0';
-        return new String(chs);
+        if (ones < n)
+            res.setCharAt(ones + zeros - 1, '0');
+        return res.toString();
+ //   }
     
     }
 }
