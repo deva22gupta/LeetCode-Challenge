6f641d82e686fa4e051346fac0298e6e0ba90ff8
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-12 20:12:32 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (13.41%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,14 @@
+class Solution {
+   // public int countStudents(int[] students, int[] sandwiches) {
+        
+        
+    public int countStudents(int[] A, int[] B) {
+        int count[] = {0, 0}, n = A.length, k;
+        for (int a: A)
+            count[a]++;
+        for (k = 0; k < n && count[B[k]] > 0; ++k)
+            count[B[k]]--;
+        return n - k;
+    }
+        
+    }
