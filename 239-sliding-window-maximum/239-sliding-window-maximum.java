f1eeecac1b00729958d4f5547ba479de82e37625
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-13 15:59:27 +0530

    Time: 33 ms (88.90%), Space: 134.9 MB (72.79%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,161 +1,44 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
-  
-  // assume nums is not null
-//   int n = nums.length;
-//   if (n == 0 || k == 0) {
-//     return new int[0];
-//   }
-  
-//   int numOfWindow = n - k + 1;
-//   int[] result = new int[numOfWindow]; // number of windows
-  
-//   for (int start = 0; start < numOfWindow; ++start) {
-//     int end = start + k - 1;
-//     int maxVal = nums[start];
-//     for (int i = start + 1; i <= end; ++i) {
-//       if (nums[i] > maxVal) { // update
-//         maxVal = nums[i];
-//       }
-//     }
-//     result[start] = maxVal;
-//   }
-  
-//   return result;
-       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // using priorityqueue
-        //public int[] maxSlidingWindow(int[] nums, int k) {
-  // assume nums is not null
-//   if (nums.length == 0 || k == 0) {
-//     return new int[0];
-//   }
-//   int n = nums.length;
-//   int[] result = new int[n - k + 1]; // number of windows
-  
-//   PriorityQueue<Integer> maxPQ = new PriorityQueue<>((o1, o2) -> (o2 - o1)); // stores values
-  
-//   for (int i = 0; i < n; ++i) {
-//     int start = i - k;
-//     if (start >= 0) {
-//       maxPQ.remove(nums[start]); // remove the out-of-bound value
-//     }
-//     maxPQ.offer(nums[i]);
-//     if (maxPQ.size() == k) {
-//       result[i - k + 1] = maxPQ.peek();
-//     }
-//   }
-//   return result;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // third method
-        // Use indices since they are unique
+        //dp
+        // my first attempt
 
   // assume nums is not null
-//   if (nums.length == 0 || k == 0) {
-//     return new int[0];
-//   }
-//   int n = nums.length;
-//   int[] result = new int[n - k + 1]; // number of windows
-  
-//   PriorityQueue<Integer> maxPQ = new PriorityQueue<>((i1, i2) -> (nums[i2] - nums[i1])); // stores values
-  
-//   for (int i = 0; i < n; ++i) {
-//     int start = i - k;
-//     if (start >= 0) {
-//       maxPQ.remove(start); // remove the out-of-bound value by index
-//     }
-//     maxPQ.offer(i);
-//     if (maxPQ.size() == k) {
-//       result[i - k + 1] = nums[maxPQ.peek()];
-//     }
-//   }
-//   return result;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      //  public int[] maxSlidingWindow(int[] nums, int k) {
-  // assume nums is not null
-//   int n = nums.length;
-//   if (n == 0 || k == 0) {
-//     return new int[0];
-//   }
-//   int[] result = new int[n - k + 1]; // number of windows
-//   Deque<Integer> win = new ArrayDeque<>(); // stores indices
-  
-//   for (int i = 0; i < n; ++i) {
-//     // remove indices that are out of bound
-//     while (win.size() > 0 && win.peekFirst() <= i - k) {
-//       win.pollFirst();
-//     }
-//     // remove indices whose corresponding values are less than nums[i]
-//     while (win.size() > 0 && nums[win.peekLast()] < nums[i]) {
-//       win.pollLast();
-//     }
-//     // add nums[i]
-//     win.offerLast(i);
-//     // add to result
-//     if (i >= k - 1) {
-//       result[i - k + 1] = nums[win.peekFirst()];
-//     }
-//   }
-//   return result;
-        
-        
-        
-      //  public int[] maxSlidingWindow(int[] nums, int k) {
-  // assume nums is not null
-  int n = nums.length;
-  if (n == 0 || k == 0) {
+  if (nums.length == 0 || k == 0) {
     return new int[0];
   }
+  int n = nums.length;
   int[] result = new int[n - k + 1]; // number of windows
-  Deque<Integer> win = new ArrayDeque<>(); // stores indices
   
+  // left & right
+  int[] left = new int[n];
+  int[] right = new int[n];
+
   for (int i = 0; i < n; ++i) {
-    // remove indices that are out of bound
-    while (win.size() > 0 && win.peekFirst() <= i - k) {
-      win.pollFirst();
+    // left
+    if (i % k == 0) { // beginning of the group
+      left[i] = nums[i];
+    } else {
+      left[i] = Math.max(left[i - 1], nums[i]);
     }
-    // remove indices whose corresponding values are less than nums[i]
-    while (win.size() > 0 && nums[win.peekLast()] < nums[i]) {
-      win.pollLast();
-    }
-    // add nums[i]
-    win.offerLast(i);
-    // add to result
-    if (i >= k - 1) {
-      result[i - k + 1] = nums[win.peekFirst()];
+    
+    // right (* to be improved)
+    int temp = (i / k + 1) * k - 1; // last of the group
+    if (temp > n - 1) temp = n - 1;
+    
+    int j = temp - i % k;
+    if (j % k == (k - 1) || j == n - 1) {
+      right[j] = nums[j];
+    } else {
+      right[j] = Math.max(right[j + 1], nums[j]);
     }
   }
+  
+  // dp
+  for (int i = 0, j = i + k - 1; j < n; ++i, ++j) {
+    result[i] = Math.max(right[i], left[j]);
+  }
+  
   return result;
 }
-
     }
