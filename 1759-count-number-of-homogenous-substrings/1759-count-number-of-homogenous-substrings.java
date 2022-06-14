2884f9c89fe3e4c9d24974fdaa3d068572b63e25
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-14 19:01:15 +0530

    Time: 20 ms (26.19%), Space: 50.9 MB (43.33%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -2,24 +2,35 @@ class Solution {
     public int countHomogenous(String s) {
         
        // int m=1e9+7;
-        int m=1000000007;
-        if(s.length()==0) return 0;
-        int sum=1;   // as 1 character itself will always be homogeneous
+//         int m=1000000007;
+//         if(s.length()==0) return 0;
+//         int sum=1;   // as 1 character itself will always be homogeneous
         
-        // else count the no of substrings from left side
+//         // else count the no of substrings from left side
         
-        int arr[] = new int[s.length()];
-        Arrays.fill(arr,1);
-        int n=s.length();
-        for(int i=1;i<n;i++){
-          //  arr[i]=1;
+//         int arr[] = new int[s.length()];
+//         Arrays.fill(arr,1);
+//         int n=s.length();
+//         for(int i=1;i<n;i++){
+//           //  arr[i]=1;
             
-            if(s.charAt(i)==s.charAt(i-1)){
-                arr[i]=(1+arr[i-1]%m);
-            }
-            sum=(sum%m+arr[i]%m);
+//             if(s.charAt(i)==s.charAt(i-1)){
+//                 arr[i]=(1+arr[i-1]%m);
+//             }
+//             sum=(sum%m+arr[i]%m);
+//         }
+//         return sum%m;
+        
+        
+        
+          int res = 0, cur = 0, count = 0, mod = 1_000_000_007;
+        for (int i = 0; i < s.length(); ++i) {
+            count = s.charAt(i) == cur ? count + 1 : 1;
+            cur = s.charAt(i);
+            res = (res + count) % mod;
         }
-        return sum%m;
+        return res;
+        
         
     }
 }
