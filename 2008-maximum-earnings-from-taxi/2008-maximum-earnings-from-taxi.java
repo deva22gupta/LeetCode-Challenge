ce202031c695b9c3ad94986c19ce2d5bdd9c74c9
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-16 02:05:09 +0530

    Time: 172 ms (26.43%), Space: 125 MB (41.41%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
@@ -1,15 +1,56 @@
 class Solution {
     public long maxTaxiEarnings(int n, int[][] rides) {
-        
-        int size = rides.length;
-        Arrays.sort(rides, (a, b)->a[1] - b[1]);
-        TreeMap<Integer, Long> dp = new TreeMap<>();
-        dp.put(0, 0L);
-        for (int[] ride : rides) {
-            long cur = dp.floorEntry(ride[0]).getValue() + (ride[1] - ride[0] + ride[2]);
-            if (cur > dp.lastEntry().getValue())
-                dp.put(ride[1], cur);
+          // we don't care about the @param n, we care about the length of the @param rides array
+        n = rides.length;
+        // Sort by the ending position of route
+        Arrays.sort(rides, Comparator.comparingInt(e -> e[1]));
+        // dp[i] -> the maximum gain for stopping at rides[i][1]
+        long[] dp = new long[n];
+        for (int i = 0; i < n; i++) {
+            long[] ride = transform(rides[i]);
+            long gain = calc(ride);
+            if (i == 0) {
+                dp[i] = calc(ride);
+                continue;
+            }
+            // either you opt in for this ride, or you don't
+            dp[i] = Math.max(gain, dp[i - 1]);
+            // binary search for the largest j where j < i and rides[j][1] <= ride[0]
+            int j = search(rides, 0, i - 1, ride[0]);
+            if (j >= 0) {
+                dp[i] = Math.max(dp[i], dp[j] + gain);
+            }
         }
-        return dp.lastEntry().getValue();
+        return dp[n - 1];
+    }
+
+    int search(int[][] rides, int l, int r, long target) {
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (rides[m][1] <= target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        if (rides[r][1] <= target) {
+            return r;
+        }
+        if (rides[l][1] <= target) {
+            return l;
+        }
+        return -1;
+    }
+
+    long calc(long[] arr) {
+        return arr[1] - arr[0] + arr[2];
+    }
+
+    long[] transform(int[] arr) {
+        long[] ret = new long[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ret[i] = (long) arr[i];
+        }
+        return ret;
     }
     }
