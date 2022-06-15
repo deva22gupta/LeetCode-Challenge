5d9912f69cecf41245cbc107769dee63cec708bd
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-15 10:55:16 +0530

    Time: 77 ms (33.43%), Space: 44.6 MB (74.77%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -23,30 +23,74 @@ class Solution {
         
         // dfs solution with memo 
           
-        int ans = 0;
-        Set<String> set = new HashSet<>();
-        Map<String, Integer> map = new HashMap<>();
-        for(String word : words) set.add(word);
-        for(String word : words) ans = Math.max(ans, helper(map, set, word));
-        return ans;
-    }
+//         int ans = 0;
+//         Set<String> set = new HashSet<>();
+//         Map<String, Integer> map = new HashMap<>();
+//         for(String word : words) set.add(word);
+//         for(String word : words) ans = Math.max(ans, helper(map, set, word));
+//         return ans;
+//     }
     
-    private int helper(Map<String, Integer> map, Set<String> set, String word){
-        if(map.containsKey(word)) return map.get(word);
-        int cnt = 0;
-        for(int i = 0; i < word.length(); i++){
-            String next = word.substring(0, i) + word.substring(i+1);
-            if(set.contains(next)){
-                cnt = Math.max(cnt, helper(map, set, next));
+//     private int helper(Map<String, Integer> map, Set<String> set, String word){
+//         if(map.containsKey(word)) return map.get(word);
+//         int cnt = 0;
+//         for(int i = 0; i < word.length(); i++){
+//             String next = word.substring(0, i) + word.substring(i+1);
+//             if(set.contains(next)){
+//                 cnt = Math.max(cnt, helper(map, set, next));
+//             }
+//         }
+//         map.put(word, 1 + cnt);
+//         return 1 + cnt;
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    //    public int longestStrChain(String[] words) {
+    Arrays.sort(words, new StringByLengthComparator());
+    int[] dp = new int[words.length];
+    int maxLen = 0;
+    for (int i = 0; i < words.length; i++) {
+        dp[i] = 1;
+        for (int j = i - 1; j >= 0 && words[i].length() - words[j].length() <= 1; j--) {
+            if (isPredecessor(words[j], words[i])) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
-        map.put(word, 1 + cnt);
-        return 1 + cnt;
-    
-        
-        
-        
+        maxLen = Math.max(maxLen, dp[i]);
+    }
+    return maxLen;
+}
+
+ private boolean isPredecessor(String s1, String s2) {
+    if (s2.length() == s1.length()) return false;
+    int diff = 0;
+    for (int i = 0, j = 0; i < s1.length(); ) {
+        if (s1.charAt(i) == s2.charAt(j)) {
+            i++;
+            j++;
+        } else {
+            diff++;
+            if (diff > 1) return false;
+            j++;
+        }
+    }
+    return true;
+}
+
+private static class StringByLengthComparator implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        return Integer.compare(s1.length(), s2.length());
+    }
+}
         
         
     }
-}
