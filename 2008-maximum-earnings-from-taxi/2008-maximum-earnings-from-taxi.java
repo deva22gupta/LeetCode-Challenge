43e6d597dfe96fd6b90e75d6027cdc9e1c6f8b43
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-16 02:00:50 +0530

    Time: 85 ms (86.65%), Space: 119.2 MB (49.04%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
@@ -1,16 +1,18 @@
-class Solution {
-    public long maxTaxiEarnings(int n, int[][] A) {
-        
-           Arrays.sort(A, (a, b) -> a[0] - b[0]);
-        long[] dp = new long[n + 1];
-        int j = 0;
-        for(int i = 1; i <= n; ++i) {
-            dp[i] = Math.max(dp[i], dp[i - 1]);
-            while (j < A.length && A[j][0] == i) {
-                dp[A[j][1]] = Math.max(dp[A[j][1]], dp[i] + A[j][1] - A[j][0] + A[j][2]);
-                ++j;
-            }
-        }
-        return dp[n];
-    }
+// Came up with a similar solution but instead of sorting on the start point, I did on the end point and found it a bit more intuitive. Also, eliminates the the first Math.max call since we are populating the dp array sequentially unlike what is done in OP's solution.
+class Solution{
+	public long maxTaxiEarnings(int n, int[][] rides) {
+		Arrays.sort(rides, (a, b) -> Integer.compare(a[1], b[1]));
+		long[] dp = new long[n + 1];
+		int k = 0;
+
+		for (int i = 1; i < dp.length; i++) {
+			dp[i] = dp[i - 1];
+			while (k < rides.length && i == rides[k][1]) {
+				int[] ride = rides[k++];
+				dp[i] = Math.max(dp[i], dp[ride[0]] + ride[1] - ride[0] + ride[2]);
+			}
+		}
+
+		return dp[n];
+	}
 }
