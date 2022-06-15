aab8830fc17160f1980fb23b0e782e904a590391
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-16 02:03:08 +0530

    Time: 133 ms (44.14%), Space: 127.3 MB (39.51%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.java
@@ -1,18 +1,15 @@
-// Came up with a similar solution but instead of sorting on the start point, I did on the end point and found it a bit more intuitive. Also, eliminates the the first Math.max call since we are populating the dp array sequentially unlike what is done in OP's solution.
-class Solution{
-	public long maxTaxiEarnings(int n, int[][] rides) {
-		Arrays.sort(rides, (a, b) -> Integer.compare(a[1], b[1]));
-		long[] dp = new long[n + 1];
-		int k = 0;
-
-		for (int i = 1; i < dp.length; i++) {
-			dp[i] = dp[i - 1];
-			while (k < rides.length && i == rides[k][1]) {
-				int[] ride = rides[k++];
-				dp[i] = Math.max(dp[i], dp[ride[0]] + ride[1] - ride[0] + ride[2]);
-			}
-		}
-
-		return dp[n];
-	}
-}
+class Solution {
+    public long maxTaxiEarnings(int n, int[][] rides) {
+        
+        int size = rides.length;
+        Arrays.sort(rides, (a, b)->a[1] - b[1]);
+        TreeMap<Integer, Long> dp = new TreeMap<>();
+        dp.put(0, 0L);
+        for (int[] ride : rides) {
+            long cur = dp.floorEntry(ride[0]).getValue() + (ride[1] - ride[0] + ride[2]);
+            if (cur > dp.lastEntry().getValue())
+                dp.put(ride[1], cur);
+        }
+        return dp.lastEntry().getValue();
+    }
+    }
