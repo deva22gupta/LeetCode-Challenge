7049a455503ad01b7668fcc28cf3565ebde9d32e
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-15 12:08:40 +0530

    Time: 137 ms (26.80%), Space: 72.3 MB (7.52%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumScore(int a, int b, int c) {
+        
+       // Given three integers a​​​​​, b,​​​​​ and c​​​​​, return the maximum score you can get.
+        
+          Queue<Integer> q = new PriorityQueue<>((n1, n2) -> n2 - n1);
+        int[] arr = new int[]{a, b, c} ;
+        for(int val : arr) {
+          q.offer(val);
+        }
+        int score = 0;
+        int m1, m2;
+        while ((m1 = q.poll()) > 0 && (m2 = q.poll()) > 0) {
+          score++;
+          q.offer(m1 - 1);
+          q.offer(m2 - 1);
+        }
+        return score;
+        
+        
+        
+    }
+}
