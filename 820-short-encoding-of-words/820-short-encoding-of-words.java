5666f234ab1006f51301df5356a2981415fb6c41
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-20 15:55:57 +0530

    Time: 505 ms (10.43%), Space: 51.5 MB (76.07%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        
+        // sorting on the basis of the length
+        // as the string with more length is likely to include in the encoded string
+        Arrays.sort(words,(a,b)-> b.length()-a.length());
+        StringBuilder encoding = new StringBuilder();
+        for(String s:words){
+            if(encoding.indexOf(s+"#")==-1){
+                encoding.append(s+'#');
+                
+            }
+        }
+        
+        return encoding.length();
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
