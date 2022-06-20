91cb32d2169de72d9bd228329b81943871d313ed
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-20 16:05:54 +0530

    Time: 21 ms (87.12%), Space: 54.3 MB (63.80%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,19 +1,49 @@
 class Solution {
     public int minimumLengthEncoding(String[] words) {
         
-        // sorting on the basis of the length
-        // as the string with more length is likely to include in the encoded string
-        Arrays.sort(words,(a,b)-> b.length()-a.length());
-        StringBuilder encoding = new StringBuilder();
-        for(String s:words){
-            if(encoding.indexOf(s+"#")==-1){
-                encoding.append(s+'#');
+//         // sorting on the basis of the length
+//         // as the string with more length is likely to include in the encoded string
+//         Arrays.sort(words,(a,b)-> b.length()-a.length());
+//         StringBuilder encoding = new StringBuilder();
+//         for(String s:words){
+//             if(encoding.indexOf(s+"#")==-1){
+//                 encoding.append(s+'#');
                 
+//             }
+//         }
+        
+//         return encoding.length();
+//         // here the TC:  O(NlogN)         SC:  O(length of array)
+        
+        
+        
+        
+        
+        //using hashset if present will remove from the set
+        HashSet<String> hs = new HashSet<>(Arrays.asList(words));
+        // already words are there in set
+        for(String str:words){
+            for(int i=1;i<str.length();i++){
+                hs.remove(str.substring(i));
             }
         }
         
-        return encoding.length();
-        // here the TC:  O(NlogN)         SC:  O(length of array)
+        
+        int len=0;
+        for(String s:hs){
+            len+=s.length()+1;  // 1 is added for the length of hash
+        }
+        
+        return len;
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
