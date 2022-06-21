77b33fa6d7b33a9246cdfe53884cc97d8f585c06
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-06-21 13:48:42 +0530

    Time: 22 ms (82.74%), Space: 51.6 MB (90.42%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -7,12 +7,14 @@ class Solution {
            PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < A.length - 1; i++) {
             int d = A[i + 1] - A[i];
-            if (d > 0)
-                pq.add(d);
+            if (d > 0){
+                 pq.add(d);
             if (pq.size() > ladders)
                 bricks -= pq.poll();
             if (bricks < 0)
                 return i;
+            }
+               
         }
         return A.length - 1;
         
