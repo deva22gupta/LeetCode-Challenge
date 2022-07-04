e15193c01218733738a1afa480a5b9b982a27531
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-07-04 16:16:02 +0530

    Time: 19 ms (18.57%), Space: 226 MB (35.47%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,44 +1,18 @@
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        // 2 array and 5 variable
+ //   public int[] rearrangeArray(int[] nums) {
         
-     int n= nums.length;
-        int arrp [] = new int[n/2];
-        int arrn[] = new int [n/2];
-        int p=0,k=0,l=0;
-        boolean flag=true;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                arrp[p]=nums[i];
-                p++;
-            }else{
-                arrn[k]=nums[i];
-                k++;
-            }
+            public int[] rearrangeArray(int[] nums) {
+        int[] result = new int[nums.length];
+        int j = 0, k = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            while (j < nums.length && nums[j] < 0) j++;
+            while (k < nums.length && nums[k] > 0) k++;
             
-        }
-        p=0;k=0;
-        while(l<nums.length){
-            if(flag){
-                
-                nums[l]=arrp[p];
-                p++;
-            }
-            else{
-                nums[l]=arrn[k];
-                k++;
-            }
-            l++;
-            flag=!flag;
+            result[i] = (i % 2 == 0) ? nums[j++] : nums[k++];
         }
         
-        
-        //3 1 -2 -3 2 -4
-        // 3 1 2
-        // -2 -3 -4
-        
-        
-        
-        return nums;
+        return result;
     }
 }
+    
