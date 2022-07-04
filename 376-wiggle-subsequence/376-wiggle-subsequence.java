179ed6b725a4bd67ab2d2e2c2dc283dcd65408d8
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-07-04 15:52:14 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (59.82%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,32 +1,23 @@
 class Solution {
     public int wiggleMaxLength(int[] a) {
-     
+  if(a.length < 2) return a.length;
         
-        // longest wiggle sequence
-        //Dp solution 
-          // but there is alinear solution which is better than a dp solution 
-        // in which you just have rto count all the peaks
-         // public static int wiggleDPSolution(int[] a) {
-        if(a.length < 2) return a.length;
-        int inc[] = new int[a.length], dec[] = new int[a.length];
-        Arrays.fill(inc,1);
-        Arrays.fill(dec,1);        
-        int ans = 1;
-        for(int i=1;i<a.length;i++) {
-            for(int j=0;j<i;j++) {
-                if(a[j] < a[i]) inc[i] = Math.max(inc[i],1+dec[j]);
-                if(a[j] > a[i]) dec[i] = Math.max(dec[i],1+inc[j]);
+        int i = 1; while(i < a.length && a[i] == a[i-1]) i++;
+        if(i == a.length) return 1;
+        
+        int c = 2;
+        boolean inc = a[i] > a[i-1];
+        while(i < a.length) {
+            if(inc) {
+                while(i < a.length && a[i] >= a[i-1]) i++;
+                if(i < a.length) c++; inc = false;
             }
-            ans = Math.max(ans,Math.max(inc[i],dec[i]));
+            else {
+                while(i < a.length && a[i] <= a[i-1]) i++;
+                if(i < a.length) c++; inc = true;
+            }
+            
         }
-        return ans;
+        return c;       
     }
-        
-        
-        
-        
-        
-        
-        
-        
-    }
+}
