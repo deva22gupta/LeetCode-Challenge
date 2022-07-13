046cd08830b4cad8374a6ebba88f39001448df41
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-07-13 16:14:13 +0530

    Time: 2 ms (78.80%), Space: 52.7 MB (39.60%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        // prefix sum checking by removing that index and checking the condition before adding it to prefix sum
+        int total=0;
+        for(int a:nums) total+=a;
+        int prefixSum=0;
+        for(int i=0;i<nums.length;i++){
+            if(prefixSum*2==total-nums[i]) return i;
+            prefixSum+=nums[i];
+        }
+        // if nothing matches 
+        return -1;
+    }
+}
