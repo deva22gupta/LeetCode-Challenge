1a4a805a63427cb787a6e694a8a626a0d679186c
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-07-16 21:04:42 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (86.09%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
-    // if(n==0 || n==1) return n;
-         if(n<2)  return n;
+    if(n==0 || n==1) return 1;
+       //  if(n<2)  return n;
         int prev=1;
         int prev2=1;
         for(int i=2;i<=n;i++){
