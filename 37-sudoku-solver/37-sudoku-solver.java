2691dd0f880fad59eab610ba40e9f430e5c7ca2a
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-07-22 19:45:09 +0530

    Time: 21 ms (42.39%), Space: 41.8 MB (39.25%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,41 +1,32 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-         
-        if(board == null || board.length == 0)
-            return;
-        solve(board);
+       public void solveSudoku(char[][] board) {
+        doSolve(board, 0, 0);
     }
     
-    public boolean solve(char[][] board){
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){//trial. Try 1 through 9
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c; //Put c for this cell
-                            
-                            if(solve(board))
-                                return true; //If it's the solution return true
-                            else
-                                board[i][j] = '.'; //Otherwise go back
-                        }
+    private boolean doSolve(char[][] board, int row, int col) {
+        for (int i = row; i < 9; i++, col = 0) { // note: must reset col here!
+            for (int j = col; j < 9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (doSolve(board, i, j + 1))
+                            return true;
+                        board[i][j] = '.';
                     }
-                    
-                    return false;
                 }
+                return false;
             }
         }
         return true;
     }
     
-    private boolean isValid(char[][] board, int row, int col, char c){
-        for(int i = 0; i < 9; i++) {
-            if(board[i][col] != '.' && board[i][col] == c) return false; //check row
-            if(board[row][i] != '.' && board[row][i] == c) return false; //check column
-            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' && 
-board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false; //check 3*3 block
-        }
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; // Block no. is i/3, first element is i/3*3
+        for (int i = 0; i < 9; i++)
+            if (board[i][col] == num || board[row][i] == num || 
+                    board[blkrow + i / 3][blkcol + i % 3] == num)
+                return false;
         return true;
     }
-    
 }
