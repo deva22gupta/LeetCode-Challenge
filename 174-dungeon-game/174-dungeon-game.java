b8f91efb54ff837e7e36b4f98a6484debc2844c5
deva22gupta <83506655+deva22gupta@users.noreply.github.com>
2022-07-24 13:14:09 +0530

    Time: 2 ms (75.51%), Space: 43.7 MB (73.75%) - LeetHub

---
+++ 174-dungeon-game/174-dungeon-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+   //     if(health<=0) // then he will die immediately
+       
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[n-1] = 0;
+
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+
+                int res = Math.max(dp[j], dp[j+1]);
+
+                if( res > 0 && dungeon[i][j] < 0 ) {
+                    dp[j] = dungeon[i][j];
+                }
+                else{
+                    dp[j] = dungeon[i][j] + res;
+                }
+            }
+        }
+        if( dp[0] > 0) return 1;
+        return 1 - dp[0];
+    }   
+    }
